850a1b84a3a3ed1d5040de516a8745d8820209fb
Marc Inglis <marcinglis91@hotmail.com>
2022-11-19 23:18:05 +1100

    my-app added to client

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return("Hello World")
+    return("Hello Jon")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0') 
